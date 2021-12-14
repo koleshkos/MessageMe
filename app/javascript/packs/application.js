a236545e14352379f1f20c0088dc8ec444c3ea34
Sergey <koleshkoser@gmail.com>
2021-12-15 02:24:23 +0300

    Beautify input box, functionality and create custom scope

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,8 +16,17 @@ ActiveStorage.start()
 scroll_bottom = function() {
   if ($('#messages').length > 0) {
     $('#messages').scrollTop($('#messages')[0].scrollHeight);
-  }
-}
+  };
+};
+
+submit_message = function() {
+  $('#message_body').on('keydown', function(e) {
+    if (e.keyCode == 13) {
+      $('button').click();
+      e.target.value = "";
+    };
+  });
+};
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
@@ -25,5 +34,6 @@ $(document).on('turbolinks:load', function() {
     $(this).closest('.message').transition('fade');
   });
   
+  submit_message();
   scroll_bottom();
 })
